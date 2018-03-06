c93cbc8b6e02e8b8be31aba1cec882c94af05b83
Eduardo Curado <dubis31@gmail.com>
2018-03-06 02:00:33 -0300

    Balance Tested 1

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ from robot.Utils import services, Plots
 def main_historical(INTERMEDIATE_INTERVAL, LONG_INTERVAL):
     TIME_DEFAULT_COUNT = 0
     coins = ['USDT_BTC', 'USDT_LTC']
-    days = 15
+    days = 90
     balance = 1000
     entry_size = balance / 10
     restored = Initializations.set_up_bd(days)
